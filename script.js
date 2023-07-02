99a0f83b99c0be87bcfe45c5aa83077a9a1e84a3
shaynalene <shaynalenep@gmail.com>
2023-07-02 18:20:45 +0800

    Added game functionalities
    Added different functions that allows player to initialize and play the game.
    
    Note: Console logs are added to test a functionality. It will be deleted later.

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  let genRndmChoice = Math.floor(Math.random() * 3) + 1; // generates a number between 1-3
+  console.log(genRndmChoice);
+
+  switch (genRndmChoice) {
+    case 1:
+      return "ROCK";
+    case 2:
+      return "PAPER";
+    case 3:
+      return "SCISSORS";
+  }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection);
+  console.log(computerSelection);
+  //Player wins
+  if (
+    (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
+    (playerSelection == "PAPER" && computerSelection == "ROCK") ||
+    (playerSelection == "SCISSORS" && computerSelection == "PAPER")
+  ) {
+    playerScore++;
+
+    return `You won! ${playerSelection} beats ${computerSelection}`;
+  }
+  // Player loses
+  else if (
+    (playerSelection == "ROCK" && computerSelection === "PAPER") ||
+    (playerSelection == "PAPER" && computerSelection === "SCISSORS") ||
+    (playerSelection == "SCISSORS" && computerSelection === "ROCK")
+  ) {
+    computerScore++;
+
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection == computerSelection) {
+    return `It's a tie! Play again!`;
+  } else {
+    return `Invalid input! Try again!`;
+  }
+}
+
+function game() {
+  let numRounds = 0;
+  while (numRounds < 5) {
+    let playerSelection = prompt("Your Turn: ").toUpperCase();
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    numRounds++;
+    console.log("Round " + numRounds);
+    console.log("Player " + playerScore);
+    console.log("Computer " + computerScore);
+  }
+  if (playerScore > computerScore) {
+    return "Congratulations! You won the game!";
+  } else if (playerScore < computerScore) {
+    return "You lost against our computer! Props to you though.";
+  } else {
+    return "You tied against the computer! Try again?";
+  }
+}
+
+console.log(game());
